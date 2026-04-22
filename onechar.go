a5cfe41cf7b1756119087ee446d049982a290faf
Michael Bag <michaelbag8@gmail.com>
2026-04-22 12:10:45 +0100

    Refactor main and add processBanner function

--- onechar.go
+++ onechar.go
@@ -5,34 +5,69 @@ import (
 	"os"
 	"strings"
 )
-
-func ParseLoad() {
-
-}
-func main() {
-	data, err := os.ReadFile("standard.txt")
+func processBanner(input, fontFile string) error {
+	data, err := os.ReadFile(fontFile)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error", err)
-		return
+		return err
 	}
-	content := string(data)
 
-	input := strings.ReplaceAll(content, "\r\n", "\n")
-
-	raw := strings.Split(input, "\n\n")
+	content := strings.ReplaceAll(string(data), "\r\n", "\n")
+	lines := strings.Split(content, "\n")
 
 	var blocks [][]string
 
-	for _, r := range raw {
-		row := strings.Split(r, "\n")
-		if len(row) < 8 {
+	const Height = 8
+	const Size = 9
+	const start = 1
+	for i := start; i+Height <= len(lines); i += Size {
+		blocks = append(blocks, lines[i:i+Height])
+	}
+
+	segments := strings.Split(input, "\\n")
+
+	for s, segment := range segments {
+		if segment == "" {
+			if s != len(segments)-1 {
+				fmt.Println()
+			}
 			continue
 		}
 
-		blocks = append(blocks, row[:8])
+		for row := 0; row < 8; row++ {
+			var result strings.Builder
+
+			for _, ch := range segment {
+				index := int(ch - ' ')
+				if index < 0 || index >= len(blocks) {
+					continue
+				}
+				result.WriteString(blocks[index][row])
+			}
+
+			fmt.Println(result.String())
+		}
 	}
 
-	for i, row := range blocks[33] {
-		fmt.Printf("row %d: %s\n", i, row)
+	return nil
+}
+
+
+func main() {
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Println("Usage: go run . <text> [banner]")
+		os.Exit(1)
+	}
+
+	fontFile := "standard.txt"
+	if len(os.Args) == 3 {
+		fontFile = os.Args[2] + ".txt"
+	}
+
+	input := os.Args[1]
+
+	err := processBanner(input, fontFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
 	}
 }
